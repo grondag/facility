2d991fc63be5d7e6b6983f99d33a1785493df17b
grondag <grondag@gmail.com>
2019-04-20 10:06:06 -0700

    Project setup

---
+++ src/main/java/grondag/smart_chest/SmartChest.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright 2019 grondag
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+package grondag.smart_chest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SmartChest implements ModInitializer {
+    public static SmartChest INSTANCE = new SmartChest();
+
+    @Override
+    public void onInitialize() {
+    }
+
+    private static Logger log;
+
+    public Logger getLog() {
+        Logger result = log;
+        if (result == null) {
+            result = LogManager.getLogger("SmartChest2K");
+            log = result;
+        }
+        return result;
+    }
+}
